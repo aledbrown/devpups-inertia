459b34065c251fcd7229790839dbfc278590fdf1
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-11-01 15:00:09 +0000

    Header Log in and Log out
    Using react sharedData from Laravel - very nice

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,17 +1,17 @@
-import { PageWrapper } from "@/components/PageWrapper";
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
-import { Search } from "@/components/Search";
-import { Shortlist } from "@/components/Shortlist";
-import { PuppiesList } from "@/components/PuppiesList";
-import { NewPuppyForm } from "@/components/NewPuppyForm";
-import { useState } from "react";
-import { Puppy } from "@/types";
+import { PageWrapper } from '@/components/PageWrapper';
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
+import { Search } from '@/components/Search';
+import { Shortlist } from '@/components/Shortlist';
+import { PuppiesList } from '@/components/PuppiesList';
+import { NewPuppyForm } from '@/components/NewPuppyForm';
+import { useState } from 'react';
+import { Puppy } from '@/types';
 // import { LoaderCircle } from "lucide-react";
 // import { getPuppies } from "@/queries";
 // import { ErrorBoundary } from "react-error-boundary";
 
-export default function App({puppies} : {puppies: Puppy[]}) {
+export default function App({ puppies }: { puppies: Puppy[] }) {
     return (
         <PageWrapper>
             <Container>
@@ -23,17 +23,24 @@ export default function App({puppies} : {puppies: Puppy[]}) {
     );
 }
 
-function Main({pups} : {pups: Puppy[]}) {
-    const [searchQuery, setSearchQuery] = useState<string>("");
+function Main({ pups }: { pups: Puppy[] }) {
+    const [searchQuery, setSearchQuery] = useState<string>('');
     const [puppies, setPuppies] = useState<Puppy[]>(pups);
 
     return (
         <main>
             <div className="mt-24 grid gap-8 sm:grid-cols-2">
-                <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+                <Search
+                    searchQuery={searchQuery}
+                    setSearchQuery={setSearchQuery}
+                />
                 <Shortlist puppies={puppies} setPuppies={setPuppies} />
             </div>
-            <PuppiesList searchQuery={searchQuery} puppies={puppies} setPuppies={setPuppies} />
+            <PuppiesList
+                searchQuery={searchQuery}
+                puppies={puppies}
+                setPuppies={setPuppies}
+            />
             <NewPuppyForm puppies={puppies} setPuppies={setPuppies} />
         </main>
     );
