9c02b89e9a1b5d35b3a61531c881c6f0582ed2bf
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-11-02 11:12:04 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
 use App\Http\Controllers\PuppyController;
-use App\Models\Puppy;
-use App\Http\Resources\PuppyResource;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
-Route::get('/', [PuppyController::class, 'index'])->name('home');
-// Route::get('/', function () {
-//     return Inertia::render('welcome', [
-//         'canRegister' => Features::enabled(Features::registration()),
-//         'puppies' => PuppyResource::collection(Puppy::get()->load(['user', 'likedBy'])),
-//     ]);
-// })->name('home');
+Route::get('/', [PuppyController::class, 'index'])->name('puppy.index');
 
+// AUTH PROTECTED ROUTES
 Route::middleware(['auth', 'verified'])->group(function () {
-
     // Update Liked Puppy
     Route::patch('puppies/{puppy}/like', [PuppyController::class, 'like'])->name('puppies.like');
 
