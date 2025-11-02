8ed7fb72b36a58daa57796dbf884818c8058a9e0
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-11-02 12:03:58 +0000

    Refactor LikeToggle to be a form as well.
    Better data processing feedback. Easier to read.

--- resources/js/components/LikeToggle.tsx
+++ resources/js/components/LikeToggle.tsx
@@ -1,29 +1,38 @@
 import { like } from '@/routes/puppies';
 import { Puppy, SharedData } from '@/types';
-import { Link, usePage } from '@inertiajs/react';
-import { Heart, LoaderCircle } from 'lucide-react';
+import { useForm, usePage } from '@inertiajs/react';
 import { clsx } from 'clsx';
+import { Heart, LoaderCircle } from 'lucide-react';
 
 export function LikeToggle({ puppy }: { puppy: Puppy }) {
     const { auth } = usePage<SharedData>().props;
+    const { processing, patch } = useForm();
+
+    function submit(e: React.FormEvent) {
+        e.preventDefault();
+        patch(like(puppy.id).url, { preserveScroll: true });
+    }
 
     return (
-        <Link
-            preserveScroll
-            method={'patch'}
-            href={like(puppy.id)}
-            className={`group ${auth.user ? 'cursor-pointer' : 'cursor-not-allowed'}`}
-            disabled={!auth?.user}
-        >
-            <LoaderCircle className="hidden animate-spin stroke-slate-300 group-data-loading:block" />
-            <Heart
-                className={clsx(
-                    puppy.likedBy.includes(auth?.user?.id) && auth?.user
-                        ? 'fill-pink-500 stroke-none'
-                        : 'stroke-slate-200 group-hover:stroke-slate-300',
-                    'group-data-loading:hidden',
+        <form onSubmit={submit} className="flex h-full">
+            <button
+                type="submit"
+                className={`group ${auth.user ? 'cursor-pointer' : 'cursor-not-allowed'}`}
+                disabled={!auth.user || processing}
+                data-loading={processing ? true : undefined}
+            >
+                {processing ? (
+                    <LoaderCircle className="animate-spin stroke-slate-300" />
+                ) : (
+                    <Heart
+                        className={clsx(
+                            puppy.likedBy.includes(auth?.user?.id) && auth?.user
+                                ? 'fill-pink-500 stroke-none'
+                                : 'stroke-slate-200 group-hover:stroke-slate-300',
+                        )}
+                    />
                 )}
-            />
-        </Link>
+            </button>
+        </form>
     );
 }
