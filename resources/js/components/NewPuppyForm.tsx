84af1331848d90c4e67b519cbe51a50b34eb2890
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-11-11 08:08:36 +0000

    Add intertia to the new pup form

--- resources/js/components/NewPuppyForm.tsx
+++ resources/js/components/NewPuppyForm.tsx
@@ -3,14 +3,18 @@ import { useForm } from '@inertiajs/react';
 import puppies from '@/routes/puppies';
 
 export function NewPuppyForm() {
-    const { post, errors  } = useForm();
+    const { post, setData, data } = useForm({
+        name: '',
+        trait: '',
+        image: null as File | null,
+    });
     return (
         <div className="mt-12 flex items-center justify-between bg-white p-8 shadow ring ring-black/5">
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
                     post(puppies.store().url, {
-                        preserveScroll: true
+                        preserveScroll: true,
                     });
                 }}
                 className="mt-4 flex w-full flex-col items-start gap-4"
@@ -20,20 +24,24 @@ export function NewPuppyForm() {
                         <label htmlFor="name">Name</label>
                         <input
                             required
+                            value={data.name}
                             className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                             id="name"
                             type="text"
                             name="name"
+                            onChange={(e) => setData('name', e.target.value)}
                         />
                     </fieldset>
                     <fieldset className="flex w-full flex-col gap-1">
                         <label htmlFor="trait">Personality trait</label>
                         <input
                             required
+                            value={data.trait}
                             className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                             id="trait"
                             type="text"
                             name="trait"
+                            onChange={(e) => setData('trait', e.target.value)}
                         />
                     </fieldset>
                     <fieldset className="col-span-2 flex w-full flex-col gap-1">
@@ -44,6 +52,12 @@ export function NewPuppyForm() {
                             id="image"
                             type="file"
                             name="image"
+                            onChange={(e) => {
+                                setData(
+                                    'image',
+                                    e.target.files ? e.target.files[0] : null,
+                                );
+                            }}
                         />
                     </fieldset>
                 </div>
