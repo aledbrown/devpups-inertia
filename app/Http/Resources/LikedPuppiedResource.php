a190f98e27e440c113a1fb3cc8f3567427ccd65c
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-11-22 14:00:27 +0000

    Update LikedPuppiedResource.php

--- app/Http/Resources/LikedPuppiedResource.php
+++ app/Http/Resources/LikedPuppiedResource.php
@@ -16,18 +16,6 @@ class LikedPuppiedResource extends JsonResource
             'name' => $this->name,
             'trait' => $this->trait,
             'imageUrl' => $this->image_url,
-            // 'likedBy' => '',
-            // 'user' => UserResource::make($this->whenLoaded('user')),
-
-            // 'id' => $this->id,
-            // 'name' => $this->name,
-            // 'trait' => $this->trait,
-            // 'image_url' => $this->image_url,
-            // 'created_at' => $this->created_at,
-            // 'updated_at' => $this->updated_at,
-            // 'liked_by_count' => $this->liked_by_count,
-            // 'likedBy_count' => $this->likedBy_count,
-            // 'user_id' => $this->user_id,
         ];
     }
 }
